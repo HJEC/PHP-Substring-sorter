a2551dbb55133a606c79985a83378166f8654a97
pizza <HJEC92@gmail.com>
2020-05-14 11:58:15 +0200

    included php.info

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 echo 'Hello';
+
+$names = ["first"=>"php", "a"=>"Arrays", "b"=>"are","c"=> "gay","d"=> "as","e"=> "heck"];
+echo "<br /> <br/> <br />";
+print_r($names);
+echo "<br/>";
+
+foreach ($names as $name){
+
+    if ($name == "php"){
+        echo "\n <h2>Big ol' sweaty dicks</h2>";
+        continue;
+    }
+    echo "<h3>${name}</h3>";
+}
