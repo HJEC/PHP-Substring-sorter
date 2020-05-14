ae51f10bdfb9492d50ea532e009a6e5e1f739ddd
pizza <HJEC92@gmail.com>
2020-05-14 17:32:27 +0200

    added footer with links

--- functions/sorter.php
+++ functions/sorter.php
@@ -1,5 +1,20 @@
 <?php
 
-function sort_String($text){
- return strtoupper($text);
+function sort_string($text){
+    $string_array = explode(" ", strtolower($text));
+    $sorted_array = [];
+    foreach($string_array as $words){
+        $letter_array = str_split($words, 1);
+        sort($letter_array);
+        foreach($letter_array as $letter){
+            if (preg_match("/[^a-zA-Z\d\s]/", $letter)){
+                continue;
+            }
+        }
+
+        array_push($sorted_array,implode("", $letter_array));
+
+
+    }
+ return implode(" ", $sorted_array);
 }
